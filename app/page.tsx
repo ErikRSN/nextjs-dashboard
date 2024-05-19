7e03f9240e5fd08e4f88b7aa6e438998a9820756
ErikRSN <driedgrapes506@gmail.com>
2024-05-19 20:54:33 +0200

    Added Style

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
+import styles from '@/app/ui/page.module.css';
+<div className={styles.shape}/>;
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
